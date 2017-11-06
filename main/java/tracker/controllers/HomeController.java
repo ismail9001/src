e1fc7ed45dd23c00d70953ca020382431a8bc359
ismail9001 <nizamov-iskander@yandex.ru>
2017-11-06 16:21:45 +0300

    fix

--- main/java/tracker/controllers/HomeController.java
+++ main/java/tracker/controllers/HomeController.java
@@ -5,12 +5,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import tracker.models.Task;
-import tracker.services.TaskService;
 import tracker.services.UserService;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Controller
 public class HomeController {
     @Autowired
